a018520196108c11b2f36f4bab4bf70c903eff9a
Angel Alejandro <231556@moron.gob.ar>
2025-09-22 12:27:14 -0300

    seguridad

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBFuibt-wLnMGCqFHxH15JCRGsUXWvMPqA",
-  authDomain: "municipio-control-partes.firebaseapp.com",
-  projectId: "municipio-control-partes",
-  storageBucket: "municipio-control-partes.firebasestorage.app",
-  messagingSenderId: "892284199857",
-  appId: "1:892284199857:web:92ff9cc673b092e7ea5390"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
